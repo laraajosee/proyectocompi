99df99cebaef0a096b0888db5c9cf4ed93016665
laraajosee <chemanuel94@gmail.com>
2021-08-17 12:59:49 -0600

    prueba

--- proyectocompi/src/proyectocompi/Proyectocompi.java
+++ proyectocompi/src/proyectocompi/Proyectocompi.java
@@ -16,9 +16,9 @@ public class Proyectocompi {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hola");
-        System.out.println("");
-                
+           System.out.println("asdasdasd");
+                        
+                        
                 
             
     }
